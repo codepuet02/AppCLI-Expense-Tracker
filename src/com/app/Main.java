dc3dd192632fd738613f73921d1664c1d9cc9c69
andres puentes <puentesdavison@gmail.com>
2025-12-05 09:24:45 -0500

    feat: implementacion total del flujo del CLI con manejo de respuestas y vistas

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -1,8 +1,22 @@
 package com.app;
 import  com.app.cli.ManagerCLI;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ManagerCLI ManagerCli = new ManagerCLI();
+
+        while (true){
+            System.out.print(">>");
+            String answer = sc.nextLine();
+            if (answer.equalsIgnoreCase("exit")){
+
+                break;
+            }
+            ManagerCli.parseCommand(answer);
+        }
+        sc.close();
 
     }
 }
